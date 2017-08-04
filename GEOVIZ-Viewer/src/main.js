72bc52e420d647e2bfed8ee806afb9504b2063c0
KuN <kun@geotricks.net>
2017-08-03 19:57:38 -0500

    Disabled Bootstrap-Vue for now

--- GEOVIZ-Viewer/src/main.js
+++ GEOVIZ-Viewer/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import BootstrapVue from 'bootstrap-vue'
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/style.scss'
 
 import App from './App'
 import router from './router'
 
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+//Vue.use(BootstrapVue)
+//Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 let GEOVIZViewer = new Vue({
