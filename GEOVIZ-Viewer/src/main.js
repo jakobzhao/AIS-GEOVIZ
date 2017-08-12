4733b9991cdcc323889072d583b4155e3098b321
KuN <kun@geotricks.net>
2017-08-12 14:15:59 -0500

    Replace bootstrap with element ui permanently

--- GEOVIZ-Viewer/src/main.js
+++ GEOVIZ-Viewer/src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
-//import BootstrapVue from 'bootstrap-vue'
-//import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-default/index.css'
 import './assets/style.scss'
-
 import App from './App'
 import router from './router'
 
-//Vue.use(BootstrapVue)
+Vue.use(ElementUI, { locale })
+
+/*//  google analytics support
+router.afterEach(function (transition) {
+  window.ga('send', {
+    hitType: 'pageview',
+    page: transition.path
+  })
+})*/
 //Vue.config.productionTip = false
 
 /* eslint-disable no-new */
