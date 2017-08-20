046bacb182693b13ec29674df772b5376ae712e5
KuN <kun@geotricks.net>
2017-08-20 18:10:53 -0500

    Add a nav menu

--- GEOVIZ-Viewer/src/App.vue
+++ GEOVIZ-Viewer/src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">GEOVIZ AIS Visualization</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Workspace</template>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item two</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3">
+        <a href="#" target="_blank">About</a>
+      </el-menu-item>
+    </el-menu>
     <router-view></router-view>
   </div>
 </template>
